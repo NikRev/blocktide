a09855b6d5d1d29b8c9bdd3f5db0ea87386e0411
NikitaRevin <nik007rev@gmail.com>
2026-02-15 15:52:56 +0300

    Initial Commit

---
+++ Тетрис/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Тетрис
+//
+//  Created by Никита  on 15.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
